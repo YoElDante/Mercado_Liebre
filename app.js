78560279b85288439d552692a8fe6cfc496c99b0
YoElDante <vivayodante@gmail.com>
2023-11-06 20:21:37 -0300

    se completo el proyecto de mercado liebre

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(8000, ()=>{
+    console.log('Servidor online en puerto 8000');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
